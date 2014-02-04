f8a0ab97fa46fd862caea1926055c12cd9cb3ea7
Wayne Young <wyoung@pager.net>
2014-02-04 14:36:12 -0600

    Update Twilio to use Jackson 2.2.

--- src/main/java/com/twilio/sdk/parser/JsonResponseParser.java
+++ src/main/java/com/twilio/sdk/parser/JsonResponseParser.java
@@ -1,12 +1,15 @@
 package com.twilio.sdk.parser;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+//import com.fasterxml.jackson.g..codehaus.jackson.JsonParseException;
+//import org.codehaus.jackson.map.JsonMappingException;
+//import org.codehaus.jackson.map.ObjectMapper;
 
 import com.twilio.sdk.TwilioRestResponse;
 
